0131819714f293d11b096cdeab7b8a4c020e6715
alex <alexandre.joyeux@nttdata.com>
2020-09-05 21:48:00 -0700

    Adding twillio env

--- lambda/helloWorld/lambda_function.py
+++ lambda/helloWorld/lambda_function.py
@@ -1,5 +1,8 @@
+import base64
 import json
 import os
+import urllib
+from urllib import request, parse
 
 def lambda_handler(event, context):
     
@@ -16,10 +19,39 @@ def lambda_handler(event, context):
     
     requestBody = json.loads(event['body'])
 
-    DB_HOST = os.environ["DB_HOST"]
-    DB_USER = os.environ["DB_USER"]
-    DB_PASS = os.environ["DB_PASS"]
-    print("Connected to %s as %s" % (DB_HOST, DB_PASS))
+    TWILIO_SMS_URL = "https://api.twilio.com/2010-04-01/Accounts/{}/Messages.json"
+
+    TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
+    TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
+    TWILIO_DEST_NUMBER = os.environ.get("TWILIO_DEST_NUMBER")
+    TWILIO_FROM_NUMBER = os.environ.get("TWILIO_FROM_NUMBER")
+
+    if not TWILIO_ACCOUNT_SID:
+        return {
+            'statusCode': 400,
+            'body': json.dumps("Unable to access Twilio Account SID.")
+        }
+    elif not TWILIO_AUTH_TOKEN:
+        return {
+            'statusCode': 400,
+            'body': json.dumps("Unable to access Twilio Auth Token.")
+        }
+    elif not TWILIO_DEST_NUMBER:
+        return {
+            'statusCode': 400,
+            'body': json.dumps("Unable to access destination number.")
+        }
+        return "The function needs a 'To' number in the format +12023351493"
+    elif not TWILIO_FROM_NUMBER:
+        return {
+            'statusCode': 400,
+            'body': json.dumps("Unable to access destination number.")
+        }
+    elif not requestBody:
+         return {
+            'statusCode': 400,
+            'body': json.dumps("Invalid body.")
+        }
     
     return {
         'statusCode': 200,
