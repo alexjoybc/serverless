32f5b074c725d3b45fb9b10dc737494b10c66722
alex <alexandre.joyeux@nttdata.com>
2020-09-15 08:54:48 -0700

    adding reading git issue

--- azure/gitIssues/__init__.py
+++ azure/gitIssues/__init__.py
@@ -6,20 +6,34 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-            print(req_body)
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
+    try:
+        req_body = req.get_json()
+        issue = req_body.get("issue")
+
+        print(issue)
+
+        smsbodyTemplate = "Issue #{}: {}\n is {} on [{}] by {}: \n{} "
+    
+        smsBody = smsbodyTemplate.format(\
+            req_body.get("issue").get("number"),\
+            req_body.get("issue").get("title"),\
+            req_body.get("issue").get("state"),\
+            req_body.get("repository").get("name"),\
+            req_body.get("issue").get("user").get("login"),\
+            req_body.get("issue").get("html_url"))
+
+        print(smsBody)
+
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            smsBody,
+            status_code=200
         )
+
+    except ValueError:
+        pass
+
+    return func.HttpResponse(
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200
+    )
